36ed0e7d8c2166e8a3b825009382cee4fba64941
Ananta Pratama <rinna@Anantas-MacBook-Pro.local>
2018-12-24 12:45:38 +0700

    Added get community api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,11 @@ const cors = require("cors")({ origin: true })
 import * as functions from "firebase-functions"
 import * as express from "express"
 import * as bodyParser from "body-parser"
+import * as admin from "firebase-admin"
+
+admin.initializeApp()
+
+import community from "./community"
 
 const app = express()
 app.use(bodyParser.json())
@@ -13,6 +18,31 @@ app.use(
 )
 
 app.use(cors)
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  )
+
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE")
+    return res.status(200).json({})
+  }
+
+  return next()
+})
+
+app.use(async (req, _res, next) => {
+  const token = req.headers.authorization
+  const decodedToken = await admin.auth().verifyIdToken(token)
+  const uid = decodedToken.uid
+  req.uid = uid
+  next()
+})
+
+app.use(community)
 
 app.get("/", (_req, res) => {
   res.json({ message: "Kanca backend is up!" })
